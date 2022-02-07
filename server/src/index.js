29299bc353a11ef1f1ea0da7dda77b4f08f6e59d
Olga K <oskozlova@gmail.com>
2022-02-07 15:22:28 +0200

    Add test logger, change file every 10 minutes

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,22 @@
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-require("./dbMongo/mongoose");
-const router = require("./router");
-const controller = require("./socketInit");
-const handlerError = require("./handlerError/handler");
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+require('./dbMongo/mongoose');
+const router = require('./router');
+const controller = require('./socketInit');
+const handlerError = require('./handlerError/handler');
+const logger = require('./config/winstonConfig');
+const expressWinston = require('express-winston');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/public", express.static("public"));
+app.use('/public', express.static('public'));
+app.use(expressWinston.logger(logger));
 app.use(router);
+app.use(expressWinston.errorLogger(logger));
 app.use(handlerError);
 
 const server = http.createServer(app);
